f50c682f2ef0bf39b322c059ec90ab445e051fb9
sarasavani <113785747+sarasavani@users.noreply.github.com>
2024-05-23 17:37:40 +0200

    Update nonna.cpp
    scusate i nomi delle variabili ma non sapevo come renderle più chiare

--- nonna.cpp
+++ nonna.cpp
@@ -1,9 +1,54 @@
-#include <iostream>
-
 #include "nonna.h"
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <fstream>
+using namespace std;
 
-int main()
+
+struct filo
 {
-    std::cout << "Hello, World!" << std::endl;
+  int centrino;
+  int gomitolo;
+};
+
+int main() {
+
+    int C;  //numero centrini
+    int G;  //numero gomitoli
+    int M;  //numero fili
+    
+    fstream in;
+    in.open("input.txt");
+    fstream out;
+    out.open("output.txt");
+
+    stack<int> centrini;    //stack contenente i centrini
+
+    in>>C>>G>>M;
+
+    for(int i=0; i<C; i++) {
+        centrini.push(i);
+    }
+
+    vector<filo> fili;  //vettore/grafo che contiene i fili
+    fili.resize(M);
+
+    for(int i=0; i<M; i++) {
+
+        int c;
+        int g;
+        in>>c>>g;
+        fili[i].centrino=c;
+        fili[i].gomitolo=g;
+
+        out<<fili[i].centrino<<" "<<fili[i].gomitolo<<endl;
+    }
+
+    in.close();
+
+    out.close();
+
     return 0;
+
 }
