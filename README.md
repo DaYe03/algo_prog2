3185a1412e306bc658d99753e7d06ceece3f8dc6
Danye <daniele.ye03@gmail.com>
2024-05-23 15:28:53 +0000

    -primo push

---
+++ README.md
@@ -0,0 +1,1 @@
+comando compilazione ed esecuzione su linux: g++ nonna.cpp grader.cpp -o my_program && ./my_program
---
+++ nonna.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "nonna.h"
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
